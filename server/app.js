a496fc15f37bf67ebf1b60951678016d4b81da2b
kerrie <kingdream90@163.com>
2020-08-01 19:51:56 +0800

    update:port

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ app.use(router);
 
 // 数据库连接
 var db = require('./db');
-var server = app.listen(8081, function () {
+var server = app.listen(18000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
